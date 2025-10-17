7eef9e5456f89cf9be6ea032e37b2c7a2bff2221
JiuYu77 <soramail@qq.com>
2025-10-17 22:44:48 +0800

    logx test

---
+++ test/logx_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	logx "github.com/JiuYu77/go-email/log"
+)
+
+func TestLogx(t *testing.T) {
+	logger := logx.NewLogger(os.Stdout, logx.DebugLevel)
+	logger.EnableColor()
+	logger.EnableTime()
+	logger.DisableLocation()
+
+	logger.Debug("123\n", 334)
+
+	logger.Debugf("%s \033[32m%s, %d\033[0m\n", "[goemail]", "123", 334)
+	logger.Debugf("\033[32m%s, %d\033[0m\n", "123", 334)
+
+	fmt.Println("------------------------")
+
+	logger.Debugln(123, 334)
+	logger.Infoln(123, 334)
+	logger.Warnln(123, 334)
+	logger.Errorln(123, 334)
+	// logger.Panicln(123, 334)
+	// logger.Fatalln(123, 334)
+}

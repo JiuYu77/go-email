f24275356a9a4112ca1ff939c2339091fc433cb8
JiuYu77 <soramail@qq.com>
2025-10-17 20:04:28 +0800

    logx

---
+++ log/loglevel.go
@@ -0,0 +1,72 @@
+package logx
+
+import (
+	"fmt"
+)
+
+type LogLevel int8
+
+const (
+	DebugLevel LogLevel = iota
+	InfoLevel
+	WarnLevel
+	ErrorLevel
+	PanicLevel // panic
+	FatalLevel // os.Exit
+)
+
+// String 返回 LogLevel 的 首字母大写 ASCII 表示形式。
+func (l LogLevel) String() string {
+	switch l {
+	case DebugLevel:
+		return "Debug"
+	case InfoLevel:
+		return "Info"
+	case WarnLevel:
+		return "Warn"
+	case ErrorLevel:
+		return "Error"
+	case PanicLevel:
+		return "Panic"
+	case FatalLevel:
+		return "Fatal"
+	default:
+		return fmt.Sprintf("LogLevel(%d)", l)
+	}
+}
+
+// LowerString 返回 LogLevel 的 小写 ASCII 表示形式。
+func (l LogLevel) LowerString() string {
+	switch l {
+	case DebugLevel:
+		return "debug"
+	case InfoLevel:
+		return "info"
+	case WarnLevel:
+		return "warn"
+	case ErrorLevel:
+		return "error"
+	case FatalLevel:
+		return "fatal"
+	default:
+		return fmt.Sprintf("loglevel(%d)", l)
+	}
+}
+
+// UpperString 返回 LogLevel 的 大写 ASCII 表示形式。
+func (l LogLevel) UpperString() string {
+	switch l {
+	case DebugLevel:
+		return "DEBUG"
+	case InfoLevel:
+		return "INFO"
+	case WarnLevel:
+		return "WARN"
+	case ErrorLevel:
+		return "ERROR"
+	case FatalLevel:
+		return "FATAL"
+	default:
+		return fmt.Sprintf("LOGLEVEL(%d)", l)
+	}
+}
---
+++ log/style.go
@@ -0,0 +1,75 @@
+package logx
+
+import "fmt"
+
+type Style uint8
+
+const (
+	// 重置
+	Reset Style = iota
+
+	// 字体样式 font style 、misc
+	Bold
+	HalfBright // 低亮
+	Italic
+	Underline
+	Blink         // 闪烁（无效）
+	_             // 占位，多余的一个，没有6
+	Reverse       // 反显
+	Blanking      // 消隐
+	StrikeThrough // 删除线
+)
+
+// 字体颜色 basic color
+const (
+	Black Style = iota + 30 // 30
+	Red
+	Green
+	Yellow
+	Blue
+	Magenta
+	Cyan
+	White
+)
+
+// 字体颜色 亮色 bright color
+const (
+	BtBlack Style = iota + 90 // 90
+	BtRed
+	BtGreen
+	BtYellow
+	BtBlue
+	BtMagenta
+	BtCyan
+	BtWhite
+)
+
+// 背景颜色 background color
+const (
+	BgBlack Style = iota + 40 // 40
+	BgRed
+	BgGreen
+	BgYellow
+	BgBlue
+	BgMagenta
+	BgCyan
+	BgWhite
+)
+
+// 将单个样式添加到字符串
+func (s Style) Add(str string) string {
+	return fmt.Sprintf("\033[%dm%s\033[0m", s, str)
+}
+
+// 为字符串一次添加多个样式
+func AddStyle(s string, opts ...Style) string {
+	if len(opts) == 0 {
+		return s
+	}
+	var res string
+	for _, v := range opts {
+		res = fmt.Sprintf("%s\x1b[%dm", res, v)
+	}
+	res = res + s + "\x1b[0m"
+	return res
+}

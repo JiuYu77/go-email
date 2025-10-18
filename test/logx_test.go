031fbe2441ab1065d95621980642668afa4b6b8d
JiuYu77 <soramail@qq.com>
2025-10-18 16:28:47 +0800

    logx rotation test

--- test/logx_test.go
+++ test/logx_test.go
@@ -10,9 +10,10 @@ import (
 
 func TestLogx(t *testing.T) {
 	logger := logx.NewLogger(os.Stdout, logx.DebugLevel)
+	// logger := logx.NewLogger(os.Stdout, logx.ErrorLevel)
 	logger.EnableColor()
 	logger.EnableTime()
-	// logger.DisableLocation()
+	// logger.SetLocation(-1)
 
 	logger.Debug("123\n", 334)
 
@@ -27,4 +28,60 @@ func TestLogx(t *testing.T) {
 	logger.Errorln(123, 334)
 	// logger.Panicln(123, 334)
 	// logger.Fatalln(123, 334)
+
+	logger.SetCallDepth(3)
+	logger.SetPrettyPrint(true)
+
+	logger.DebugJson("This is a json data.", nil)
+	logger.DebugJson(
+		"This is a json data!",
+		logx.Fields{
+			"0": 0,
+			"1": map[string]any{"1.1": 1.1, "1.2": "1.2"},
+		},
+	)
+}
+
+func TestLogxFile(t *testing.T) {
+	name := "../tmp/app.log.jsonl"
+	file, err := os.OpenFile(name, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+	// file, err := os.OpenFile(name, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		file, err = os.Create(name)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// logger := logx.NewLogger(file, logx.DebugLevel)
+	// logger.SetCallDepth(4)
+
+	logger := logx.NewJsonLogger(file, logx.DebugLevel)
+	// logger := logx.NewJsonLogger(file, logx.InfoLevel)
+
+	// logger.SetPrettyPrint(true)
+
+	logger.DebugJson("This is a json data.", nil)
+	logger.DebugJson(
+		"This is a json data!",
+		logx.Fields{
+			"0": 0,
+			"1": map[string]any{"1.1": 1.1, "1.2": "1.2"},
+		},
+	)
+}
+
+func TestLogxRotation(t *testing.T) {
+	logger, err := logx.NewJsonRotation(logx.DebugLevel, "../tmp/logx", 1024, 3)
+	if err != nil {
+		t.Errorf("NewJsonRotation error: %v", err)
+	}
+	logger.DebugJson("This is a json Rotation data", nil)
+	logger.DebugJson(
+		"This is a json Rotation data!",
+		logx.Fields{
+			"0": 0,
+			"1": map[string]any{"1.1": 1.1, "1.2": "1.2"},
+		},
+	)
 }

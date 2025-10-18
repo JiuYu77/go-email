2d54f59a3284c8807b8a42845f5cd8e0c912617a
JiuYu77 <soramail@qq.com>
2025-10-18 17:20:28 +0800

    now_str function

--- log/types.go
+++ log/types.go
@@ -5,3 +5,10 @@ import "time"
 const (
 	DateTime = time.DateTime + ".000"
 )
+
+func now_str(layout string) string {
+	if layout == "" {
+		layout = DateTime
+	}
+	return time.Now().Format(layout)
+}
